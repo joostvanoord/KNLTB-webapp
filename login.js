ec655f2e6aee3cbd56b3ef6dc89d7476bd1df01c
Joost van Oord <joost@Joosts-MacBook-Air.local>
2026-05-28 18:26:56 +0200

    Shrink login state

--- login.js
+++ login.js
@@ -2,4 +2,4 @@ import{chromium}from"playwright";
 let b=await chromium.launch({headless:false}),p=await b.newPage();
 await p.goto("https://mijnknltb.toernooi.nl/");
 console.log("Log in, kom terug, druk Enter.");
-process.stdin.once("data",async()=>{await p.context().storageState({path:"state.json"});await b.close();console.log(Buffer.from(await import("fs").then(x=>x.readFileSync("state.json"))).toString("base64"));process.exit()});
+process.stdin.once("data",async()=>{let s=await p.context().storageState();s.origins=[];await import("fs").then(x=>x.writeFileSync("state.json",JSON.stringify(s)));await b.close();console.log(Buffer.from(JSON.stringify(s)).toString("base64"));process.exit()});
